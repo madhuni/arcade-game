798b0313b85553e0cde6c176d0842e336b8cc3c4
madhuni <kanishkamohanmadhuni@gmail.com>
2017-04-24 06:04:59 +0100

    1) 'checkCollisionBugs' function has been implemented in the 'player' prototype object. 2) Height and width of the enemy and player object have been changed. 3) 'checkCollisionBugs' function has been invoked in the "update" function of player object.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* implementing function to draw the boxes over the objects in game board */
 function drawBox(x, y, width, height, color) {
     ctx.beginPath();
     ctx.rect(x, y, width, height);
@@ -8,30 +9,33 @@ function drawBox(x, y, width, height, color) {
 
 /*********************** Enemies *****************************/
 
-var Enemy = function(x, y, speed) {
-    
+var Enemy = function (x, y, speed) {
+
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
+    this.width = 100;
+    this.height = 67;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
-    if(this.x > 505) {
+    if (this.x > 505) {
         this.x = 0;
     }
+    /*this.checkCollisionsBugs();*/
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    drawBox(this.x, this.y + 77, 100, 67, "yellow");
+    drawBox(this.x, this.y + 77, 101, 67, "yellow");
 };
 
 // Initializing the "allEnemies" array. It will consist of all the enemies objects.
@@ -39,7 +43,7 @@ var allEnemies = [];
 
 allEnemies.push(new Enemy(-300, 60, 200));
 allEnemies.push(new Enemy(-200, 145, 100));
-allEnemies.push(new Enemy(-100, 230, 350));
+allEnemies.push(new Enemy(-100, 230, 50));
 
 
 /*********************** Player ******************************/
@@ -48,40 +52,46 @@ allEnemies.push(new Enemy(-100, 230, 350));
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(speed) {
+var Player = function (speed) {
     this.sprite = 'images/char-boy.png';
     this.x = 202;
     this.y = 315;
+    this.width = 70;
+    this.height = 75;
 };
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    drawBox(this.x+17, this.y + 63, 67, 75, "blue");
+    drawBox(this.x + 17, this.y + 63, 70, 75, "blue");
 };
 
-Player.prototype.update = function() {
-    
+Player.prototype.update = function () {
+
     /* Cheking player should not move out screen */
-    
+
     if (this.x > 404) {
         this.x = 404;
     }
-    
+
     if (this.x < 0) {
         this.x = 0;
     }
-    
+
     if (this.y > 400) {
         this.y = 400;
     }
-    
-    if( this.y < 60) {
+
+    if (this.y < 60) {
         this.reset();
     }
+
+    /* Invoking the collison detection function */
+
+    this.checkCollisionsBugs();
 };
 
-Player.prototype.handleInput = function(key) {
-    
+Player.prototype.handleInput = function (key) {
+
     if (key === 'left') {
         this.x = this.x - 101;
     }
@@ -93,20 +103,28 @@ Player.prototype.handleInput = function(key) {
     }
     if (key === 'down') {
         this.y = this.y + 85;
-    } 
+    }
 };
 
-Player.prototype.reset = function() {
-    
+Player.prototype.reset = function () {
+
     this.x = 202;
     this.y = 315;
 };
 
-Player.prototype.checkCollisionsBugs = function() {
-    for(var i =0; i< allEnemies.length; i++) {
+/* Creating the Collision Detection function for player */
+Player.prototype.checkCollisionsBugs = function () {
+
+    for (var i = 0; i < allEnemies.length; i++) {
         var enemy = allEnemies[i];
-        
-        
+        if (this.x < enemy.x + enemy.width &&
+            this.x + this.width > enemy.x &&
+            this.y < enemy.y + enemy.height &&
+            this.height + this.y > enemy.y) {
+            // collision detected!
+            console.log('Collision detected !!!');
+            player.reset();
+        }
     }
 };
 
@@ -121,7 +139,7 @@ var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
