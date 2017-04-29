37e955cc32b748a67ca7be875aea9faca864ed45
madhuni <kanishkamohanmadhuni@gmail.com>
2017-04-29 10:41:36 +0100

    The new enemy image has been introduced.

--- js/engine.js
+++ js/engine.js
@@ -183,6 +183,7 @@ var Engine = (function (global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+        'images/enemy-bug-revert.png',
         'images/char-boy.png',
         'images/char-princess-girl.png',
         'images/char-pink-girl.png',
